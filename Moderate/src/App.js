d2e84653d4a4ac314d19e15401a9497403496071
Dan L <daniel.lawless@swyftx.com.au>
2022-07-27 10:15:36 +1000

    Working but stream overload

--- Moderate/src/App.js
+++ Moderate/src/App.js
@@ -13,27 +13,58 @@ function App() {
   // Switching to a Media Source rather than a URL for the video location
 
 
-  let vidURL = `https://video-bucket-aws.s3.ap-southeast-2.amazonaws.com/${quality}/videoplayback.mp4`
-  const mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-    if (videoRef.current && MediaSource.isTypeSupported(mimeCodec)) {
-      const myMediaSource = new MediaSource();
-      const source = URL.createObjectURL(myMediaSource)
+  let vidURL = `https://video-bucket-aws.s3.ap-southeast-2.amazonaws.com/${quality}/video/videoplayback_${quality}_video.webm`
+  let getVid = (async () => {
 
-      videoRef.current.src = source;
+    // Create a MediaSource instance and connect it to video element
+    const mediaSource = new MediaSource();
+    // This creates a URL that points to the media buffer,
+    // and assigns it to the video element src
+    vid.src = URL.createObjectURL(mediaSource);
   
-    myMediaSource.addEventListener('sourceopen', () => {
-      const audioSourceBuffer = myMediaSource
-      .addSourceBuffer('audio/mp4; codecs="mp4a.40.2"');
-
-      const videoSourceBuffer = myMediaSource
-      .addSourceBuffer('video/mp4; codecs="avc1.64001e"');
-
-      fetch(vidURL).then((response) => {
-        return response.arrayBuffer();
-    }).then((videoData) => {
-        videoSourceBuffer.appendBuffer(videoData);
-    });  
-  })}
+    // Video that will be fetched and appended
+  
+    // Fetch remote URL, getting contents as binary blob
+    const vidBlob = await (await fetch(vidURL)).blob();
+      console.log(vidURL)
+    // We need array buffers to work with media source
+    const vidBuff = await vidBlob.arrayBuffer();
+  
+    /**
+     * Before we can actually add the video, we need to:
+     *  - Create a SourceBuffer, attached to the MediaSource object
+     *  - Wait for the SourceBuffer to "open"
+     */
+    /** @type {SourceBuffer} */
+    const sourceBuffer = await new Promise((resolve, reject) => {
+      const getSourceBuffer = () => {
+        try {
+          const sourceBuffer = mediaSource.addSourceBuffer(`video/webm; codecs="vp9,opus"`);
+          resolve(sourceBuffer);
+        } catch (e) {
+          reject(e);
+        }
+      };
+      if (mediaSource.readyState === 'open') {
+        getSourceBuffer();
+      } else {
+        mediaSource.addEventListener('sourceopen', getSourceBuffer);
+      }
+    });
+  
+    // Now that we have an "open" source buffer, we can append to it
+    sourceBuffer.appendBuffer(vidBuff);
+    // Listen for when append has been accepted and
+    // You could alternative use `.addEventListener` here instead
+    sourceBuffer.onupdateend = () => {
+      // Nothing else to load
+      mediaSource.endOfStream();
+      // Start playback!
+      // Note: this will fail if video is not muted, due to rules about
+      // autoplay and non-muted videos
+      vid.play();
+    };
+  });
 
   useEffect(()=>{ 
     // Skip proccess on first time render
@@ -41,6 +72,7 @@ function App() {
       vid = document.getElementById('video');
 
     }
+      getVid();
       videoRef.current?.load();
       vid.currentTime = timestamp;
     
